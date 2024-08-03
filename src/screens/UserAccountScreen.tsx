6551d17819b7484a134047c4350a45ccedb89248
PasinduPabasaraPitigala <122001529+PasinduPabasaraPitigala@users.noreply.github.com>
2024-08-03 13:53:33 +0530

    updated UserAccountScreen.tsx file

--- src/screens/UserAccountScreen.tsx
+++ src/screens/UserAccountScreen.tsx
@@ -1,103 +1,87 @@
-// import * as React from 'react';
-// import { Text, View, StyleSheet, StatusBar, Image, ScrollView } from "react-native";
-// import {COLOR, FONTFAMILY, FONTSIZE, SPACING} from "../Themes/themes.tsx";
-// import AppHeader from "../components/utils/AppHeader.tsx";
-// import SettingComponent from "../components/utils/SettingComponent.tsx";
-//
-// const UserAccountScreen = ({navigation}: any) => {
-//   return (
-//     <ScrollView>
-//       <View style={styles.container}>
-//         <StatusBar hidden />
-//         <View style={styles.appHeaderContainer}>
-//           <AppHeader
-//             name="close"
-//             header={'My Profile'}
-//             action={() => navigation.goBack()}
-//           />
-//         </View>
-//
-//         <View style={styles.profileContainer}>
-//           <Image
-//             source={require('../assets/image/avatar.png')}
-//             style={styles.avatarImage}
-//           />
-//           <Text style={styles.avatarText}>John Doe</Text>
-//         </View>
-//
-//         <View style={styles.profileContainer}>
-//           <SettingComponent
-//             icon="user"
-//             heading="Account"
-//             subheading="Edit Profile"
-//             subtitle="Change Password"
-//           />
-//           <SettingComponent
-//             icon="setting"
-//             heading="Settings"
-//             subheading="Theme"
-//             subtitle="Permissions"
-//           />
-//           <SettingComponent
-//             icon="dollar"
-//             heading="Offers & Refferrals"
-//             subheading="Offer"
-//             subtitle="Refferrals"
-//           />
-//           <SettingComponent
-//             icon="info"
-//             heading="About"
-//             subheading="About Movies"
-//             subtitle="more"
-//           />
-//         </View>
-//       </View>
-//     </ScrollView>
-//
-//   );
-// };
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     display: 'flex',
-//     flex: 1,
-//     backgroundColor: COLOR.Black,
-//   },
-//   appHeaderContainer: {
-//     marginHorizontal: SPACING.space_36,
-//     marginTop: SPACING.space_20 * 2,
-//   },
-//   profileContainer: {
-//     alignItems: 'center',
-//     padding: SPACING.space_36,
-//   },
-//   avatarImage: {
-//     height: 80,
-//     width: 80,
-//     borderRadius: 80,
-//   },
-//   avatarText: {
-//     fontFamily: FONTFAMILY.poppins_medium,
-//     fontSize: FONTSIZE.size_16,
-//     marginTop: SPACING.space_16,
-//     color: COLOR.White,
-//   },
-// });
-//
-// export default UserAccountScreen;
+import * as React from 'react';
+import { Text, View, StyleSheet, StatusBar, Image, ScrollView } from "react-native";
+import {COLOR, FONTFAMILY, FONTSIZE, SPACING} from "../Themes/themes.tsx";
+import AppHeader from "../components/utils/AppHeader.tsx";
+import SettingComponent from "../components/utils/SettingComponent.tsx";
 
-
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
-export default class UserAccountScreen extends Component {
-  render() {
-    return (
-        <View>
-          <Text> textInComponent </Text>
+const UserAccountScreen = ({navigation}: any) => {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar hidden />
+        <View style={styles.appHeaderContainer}>
+          <AppHeader
+            name="close"
+            header={'My Profile'}
+            action={() => navigation.goBack()}
+          />
         </View>
-    )
-  }
-}
 
-const styles = StyleSheet.create({})
+        <View style={styles.profileContainer}>
+          <Image
+            source={require('../assets/image/avatar.png')}
+            style={styles.avatarImage}
+          />
+          <Text style={styles.avatarText}>John Doe</Text>
+        </View>
+
+        <View style={styles.profileContainer}>
+          <SettingComponent
+            icon="user"
+            heading="Account"
+            subheading="Edit Profile"
+            subtitle="Change Password"
+          />
+          <SettingComponent
+            icon="setting"
+            heading="Settings"
+            subheading="Theme"
+            subtitle="Permissions"
+          />
+          <SettingComponent
+            icon="dollar"
+            heading="Offers & Refferrals"
+            subheading="Offer"
+            subtitle="Refferrals"
+          />
+          <SettingComponent
+            icon="info"
+            heading="About"
+            subheading="About Movies"
+            subtitle="more"
+          />
+        </View>
+      </View>
+    </ScrollView>
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLOR.Black,
+  },
+  appHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_20 * 2,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    padding: SPACING.space_36,
+  },
+  avatarImage: {
+    height: 80,
+    width: 80,
+    borderRadius: 80,
+  },
+  avatarText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_16,
+    marginTop: SPACING.space_16,
+    color: COLOR.White,
+  },
+});
+
+export default UserAccountScreen;
