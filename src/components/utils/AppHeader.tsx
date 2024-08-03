9ac05ce424ac63dc998c0d8fb4d36af7a7f21b38
PasinduPabasaraPitigala <122001529+PasinduPabasaraPitigala@users.noreply.github.com>
2024-08-03 13:40:27 +0530

    add AppHeader.tsx file

---
+++ src/components/utils/AppHeader.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import CustomIcon from '../CustomIcon.ts';
+import {
+  BORDERRADIUS,
+  COLOR,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from "../../Themes/themes.tsx";
+
+const AppHeader = (props: any) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.iconBG} onPress={() => props.action()}>
+        <CustomIcon name={props.name} style={styles.iconStyle} />
+      </TouchableOpacity>
+      <Text style={styles.headerText}>{props.header}</Text>
+      <View style={styles.emptyContainer}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconStyle: {
+    color: COLOR.White,
+    fontSize: FONTSIZE.size_24,
+  },
+  headerText: {
+    flex: 1,
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_20,
+    textAlign: 'center',
+    color: COLOR.White,
+  },
+  emptyContainer: {
+    height: SPACING.space_20 * 2,
+    width: SPACING.space_20 * 2,
+  },
+  iconBG: {
+    height: SPACING.space_20 * 2,
+    width: SPACING.space_20 * 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: BORDERRADIUS.radius_20,
+    backgroundColor: COLOR.Orange,
+  },
+});
+
+export default AppHeader;
